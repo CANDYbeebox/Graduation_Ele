69974cade2681f50cc4ad8a376de5378cd61603f
SURE <707849011@qq.com>
2019-05-17 13:55:02 +0800

    test类

---
+++ src/main/java/com/black/common/Test.java
@@ -0,0 +1,11 @@
+package com.black.common;
+
+/**
+ * Created by try on 2019/5/17.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("sdjosdj");
+    }
+}
